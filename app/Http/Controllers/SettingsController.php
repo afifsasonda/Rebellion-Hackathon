3e4e3f1a9689f653d7c0ba2b081c098cc316c671
Gevano Randhi Pilko <gevano99@gmail.com>
2024-02-26 02:19:06 +0700

    Update profile settings

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class SettingsController extends Controller
+{
+    //
+    public function edit(Request $request): View
+    {
+        return view('profile.settings', [
+            'user' => $request->user(),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
-use App\Http\Controllers\AktivitasController;
-use App\Http\Controllers\BerandaprofileController;
-use App\Http\Controllers\JenisketerampilanController;
-use App\Http\Controllers\JenispengalamanController;
-use App\Http\Controllers\KeterampilanController;
-use App\Http\Controllers\PengalamanController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\AktivitasController;
+use App\Http\Controllers\PengalamanController;
+use App\Http\Controllers\KeterampilanController;
+use App\Http\Controllers\BerandaprofileController;
+use App\Http\Controllers\JenispengalamanController;
+use App\Http\Controllers\JenisketerampilanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/settings', [SettingsController::class, 'edit'])->name('settings.edit');
 
     Route::resource('posts',PostController::class);
     Route::resource('pengalaman',PengalamanController::class);
